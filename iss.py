bfd343ed336329ab3ec40e40f401d25e7f36e02a
jayaimzzz <jameshorton@gmail.com>
2019-03-14 14:06:02 -0400

    part A done

--- iss.py
+++ iss.py
@@ -1,11 +1,15 @@
 #!/usr/bin/env python
 
-__author__ = '???'
+__author__ = "jayaimzzz"
 
+import requests
 
 def main():
-    pass
-
+    r = requests.get("http://api.open-notify.org/astros.json")
+    data = r.json()
+    for people in data["people"]:
+        print "{} is currently on board the {}".format(people["name"],people["craft"])
+    print "There are {} astronauts in space".format(len(data["people"]))
 
 if __name__ == '__main__':
     main()
